ba1bc8c96ccd5cfe78e7cbc3a1ff41289d63ce2d
Nolan Law <nolan.law@yahoo.com>
2025-06-27 17:56:23 -0500

    Initial Commit

---
+++ Windburn Hub/Windburn_HubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Windburn_HubApp.swift
+//  Windburn Hub
+//
+//  Created by Nolan Law on 2025-06-27.
+//
+
+import SwiftUI
+
+@main
+struct Windburn_HubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
